f2dabf94f1034b34704d333eaf118c0a6a522c31
Ilya Pcn <pchelinilya13@gmail.com>
2023-01-22 15:41:21 +1100

    feat: add main func

--- src/index.js
+++ src/index.js
@@ -35,12 +35,21 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' '
 };
 
-function decode(expr) {
-    // write your solution here
-}
+const decode = expr => expr.split('')
+    .map( (item, i, arr ) => i % 2 === 0? item = item + arr[i + 1] : item)
+    .filter( item => item.length > 1)
+    .map( item => item === '00' ? item = '' : item === '**' ? item = ' ' : item === '10' ? item = '.' : item === '11' ? item = '-' : item)
+    .map( ( item => [item]))
+    .map((item , i ,arr) => i % 5 === 0 ? item : arr[i - (i %5)].push(item))
+    .map( item => item.length > 1? item.flat(): item )
+    .filter( item => item.length > 1)
+    .map( item => item.reduce( (acc, cur) => acc + cur))
+    .map( item => item = MORSE_TABLE[item])
+    .join('');
 
-module.exports = {
-    decode
-}
+module.exports = {decode}
+
+// я уже делал это задание на stage 0, набор декабрь 21го
